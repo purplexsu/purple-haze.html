b064f09e9ae0b3c054b6ec8669d5802bc96656e1
Purplexsu@gmail.com <Purplexsu@gmail.com>
2015-04-09 05:08:25 +0800

    Tweak the captcha image.

--- captcha.php
+++ captcha.php
@@ -5,9 +5,8 @@ $captcha=strval(rand("1111111","9999999"));
 $_SESSION["captcha"]=$captcha;
 $img=imagecreate(70,20);
 Header("C3ontent-type:image/png");
-$black=ImageColorAllocate($img,0,0,0);
 $white=ImageColorAllocate($img,239,239,239);
-$gray=ImageColorAllocate($img,180,170,170); 
+$gray=ImageColorAllocate($img,150,150,150); 
 imagefill($img,0,0,$white);
 imagestring($img,5,4,1,$captcha,$gray);
 for($i=0;$i<150;$i++)
